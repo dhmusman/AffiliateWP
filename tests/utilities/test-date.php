20afc9ba06ac342115137fd2b2e43abcdc2ab869
Drew Jaynes <drew@affiliatewp.com>
2017-08-24 16:46:37 -0600

    Add tests for all Date properties.

--- tests/utilities/test-date.php
+++ tests/utilities/test-date.php
@@ -45,5 +45,41 @@ class Tests extends UnitTestCase {
 		self::$date = self::$utils->date;
 	}
 
+	/**
+	 * @covers \AffWP\Utils\Date::$timezone
+	 */
+	public function test_timezone_prop_should_contain_the_WP_timezone() {
+		$timezone = self::$date->get_core_timezone();
+
+		$this->assertSame( $timezone, self::$date->timezone );
+	}
+
+	/**
+	 * @covers \AffWP\Utils\Date::$date_format
+	 */
+	public function test_date_format_should_contain_the_WP_date_format() {
+		$date_format = get_option( 'date_format', 'M j, Y' );
+
+		$this->assertSame( $date_format, self::$date->date_format );
+	}
+
+	/**
+	 * @covers \AffWP\Utils\Date::$time_format
+	 */
+	public function test_time_format_should_contain_the_WP_time_format() {
+		$time_format = get_option( 'time_format', 'g:i a' );
+
+		$this->assertSame( $time_format, self::$date->time_format );
+	}
+
+	/**
+	 * @covers \AffWP\Utils\Date::$datetime_format
+	 */
+	public function test_datetime_format_should_contain_the_WP_date_and_time_formats() {
+		$date_format = get_option( 'date_format', 'M j, Y' );
+		$time_format = get_option( 'time_format', 'g:i a' );;
+
+		$this->assertSame( $date_format . ' ' . $time_format, self::$date->datetime_format );
+	}
 
 }

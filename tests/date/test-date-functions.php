1c8a6b59adc77567d897c2443f8a9c1e2bc74fba
Drew Jaynes <drew@affiliatewp.com>
2017-08-24 19:05:04 -0600

    Stub out empty tests for affwp_get_filter_dates().

--- tests/date/test-date-functions.php
+++ tests/date/test-date-functions.php
@@ -10,5 +10,159 @@ use AffWP\Tests\UnitTestCase;
  * @group dates
  */
 class Tests extends UnitTestCase {
-	
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_should_return_array_with_string_dates_for_this_month_by_default() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_values_objects_should_return_array_with_object_dates_for_this_month_by_default() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_last_month_date_range_values_strings_should_return_string_dates_for_last_month() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_last_month_date_range_values_objects_should_return_object_dates_for_last_month() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_today_date_range_should_return_string_dates_for_today() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_today_date_range_values_objects_should_return_object_dates_for_today() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_yesterday_date_range_should_return_string_dates_for_yesterday() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_yesterday_date_range_values_objects_should_return_object_dates_for_yesterday() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_this_week_date_range_should_return_string_dates_for_last_week() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_this_week_date_range_values_objects_should_return_object_dates_for_last_week() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_this_quarter_date_range_should_return_string_dates_for_this_quarter() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_this_quarter_date_range_values_objects_should_return_object_dates_for_this_quarter() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_last_quarter_date_range_should_return_string_dates_for_last_quarter() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_last_quarter_date_range_values_objects_should_return_object_dates_for_last_quarter() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_this_year_date_range_should_return_string_dates_for_this_year() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_this_year_date_range_values_objects_should_return_object_dates_for_this_year() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_last_year_date_range_should_return_string_dates_for_last_year() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_last_year_date_range_values_objects_should_return_object_dates_for_last_year() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_other_date_range_should_return_string_dates_for_filter_date_values() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_other_date_range_values_objects_should_return_object_dates_for_filter_date_values() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_empty_date_range_should_return_string_dates_for_filter_date_values() {
+
+	}
+
+	/**
+	 * @covers ::affwp_get_filter_dates()
+	 */
+	public function test_get_filter_dates_with_empty_date_range_values_objects_should_return_object_dates_for_filter_date_values() {
+
+	}
+
 }

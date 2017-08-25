992d02bbe44fa6ec0320b06730b45f4794a5fe58
Drew Jaynes <drew@affiliatewp.com>
2017-08-24 18:40:10 -0600

    Stub out date-functions test class.

---
+++ tests/date/test-date-functions.php
@@ -0,0 +1,14 @@
+<?php
+namespace AffWP\Utils\Date\Functions;
+
+use AffWP\Tests\UnitTestCase;
+
+/**
+ * Tests for includes/date-functions.php
+ *
+ * @group functions
+ * @group dates
+ */
+class Tests extends UnitTestCase {
+	
+}

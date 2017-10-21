d9697d053794f47dc15d798563f98b31c9882f63
Drew Jaynes <drew@affiliatewp.com>
2017-10-21 14:52:38 -0600

    Add the reCAPTCHA field to the login form.

--- templates/login.php
+++ templates/login.php
@@ -37,6 +37,16 @@ affiliate_wp()->login->print_errors();
 			</label>
 		</p>
 
+		<?php if ( affwp_is_recaptcha_enabled() ) :
+			affwp_enqueue_script( 'affwp-recaptcha' ); ?>
+
+			<div class="g-recaptcha" data-sitekey="<?php echo esc_attr( affiliate_wp()->settings->get( 'recaptcha_site_key', '' ) ); ?>"></div>
+
+			<p>
+				<input type="hidden" name="g-recaptcha-remoteip" value="<?php echo esc_attr( affiliate_wp()->tracking->get_ip() ); ?>" />
+			</p>
+		<?php endif; ?>
+
 		<p>
 			<input type="hidden" name="affwp_redirect" value="<?php echo esc_url( $affwp_login_redirect ); ?>"/>
 			<input type="hidden" name="affwp_login_nonce" value="<?php echo wp_create_nonce( 'affwp-login-nonce' ); ?>" />
